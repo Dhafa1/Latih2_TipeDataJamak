3c425e2e738c582ff3f4c9e2123eff672c224756
Dhafa Bintang <151533878+Dhafa1@users.noreply.github.com>
2024-07-02 11:41:39 +0700

    Update Program.cs

--- Latih2_TipeDataJamak/Program.cs
+++ Latih2_TipeDataJamak/Program.cs
@@ -4,28 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /* // Deklarasi dan inisialisasi Dictionary
-             Dictionary<string, int> nilaiSiswa = new Dictionary<string, int>
-         {
-             { "Budi", 85 },
-             { "Siti", 90 },
-             { "Adi", 78 }
-         };
-
-             // Akses nilai berdasarkan kunci
-             Console.WriteLine("Nilai Budi: " + nilaiSiswa["Budi"]);
-
-             Console.WriteLine("Nilai Siti:" + nilaiSiswa["Siti"]);
-             Console.WriteLine("Niai Adi: " + nilaiSiswa["Adi"]);
-
-             // Menambah pasangan kunci-nilai baru
-             nilaiSiswa["Dina"] = 92;
-             Console.WriteLine("Nilai Dina: " + nilaiSiswa["Dina"]);
-
-             // Mengubah nilai berdasarkan kunci
-             nilaiSiswa["Adi"] = 80;
-             Console.WriteLine("Nilai Adi setelah diubah: " + nilaiSiswa["Adi"]);*/
-
+          
             Dictionary<string, int> nilaimurid = new Dictionary<string, int>
             {
                 { "Budi", 86 },
